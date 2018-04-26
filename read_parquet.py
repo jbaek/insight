561cdef376e7cd503c733d9b2744d8df7b884d7d
Ubuntu <ubuntu@ip-10-0-0-10.us-west-2.compute.internal>
2018-04-26 23:15:23 +0000

    sentence_segment working after jar path specified

--- read_parquet.py
+++ read_parquet.py
@@ -12,8 +12,11 @@ from sparknlp.base import DocumentAssembler, Finisher
 
 spark = SparkSession.builder.appName("ner").master("local[1]").config("spark.driver.memory","8G").config("spark.driver.maxResultSize", "2G").config("spark.jar", "lib/sparknlp.jar").config("spark.kryoserializer.buffer.max", "500m").getOrCreate()
 
+
 #Load the input data to be annotated
 data = spark.read.parquet("sentiment.parquet").limit(1000)
 data.cache()
 data.count()
 data.show()
+
+time.sleep(60)
--- sentence_segment.py
+++ sentence_segment.py
@@ -4,8 +4,8 @@ import os
 
 import glob
 sys.path.extend(glob.glob(os.path.join(os.path.expanduser("~"), ".ivy2/jars/*.jar")))
-from sparknlp.base import *
-from sparknlp.annotator import *
+from sparknlp.base import DocumentAssembler, Finisher
+from sparknlp.annotator import SentenceDetector
 from sparknlp.common import *
 from pyspark.sql import SparkSession
 from pyspark.ml import Pipeline
@@ -18,12 +18,12 @@ from pyspark.ml import Pipeline
 
 spark = SparkSession.builder.appName("ner").master("local[1]").config("spark.driver.memory","8G").config("spark.driver.maxResultSize", "2G").config("spark.jar", "lib/sparknlp.jar").config("spark.kryoserializer.buffer.max", "500m").getOrCreate()
 
-sentenceData = spark.createDataFrame([(1, "Hi I heard about Spark"), (2, "I wish Java could use case classes"), (3, "Logistic regression models are neat")], ["id", "rawDocument"])
+sentence_data = spark.createDataFrame([(1, "Hi I heard about Spark"), (2, "I wish Java could use case classes"), (3, "Logistic regression models are neat")], ["id", "rawDocument"])
 
-documentAssembler = DocumentAssembler().setInputCol("rawDocument").setOutputCol("document")
+document_assembler = DocumentAssembler().setInputCol("rawDocument").setOutputCol("document")
 sentence_detector = SentenceDetector().setInputCols(["document"]).setOutputCol("sentence")
 
-pipeline = Pipeline().setStages([documentAssembler, sentence_detector])
+pipeline = Pipeline().setStages([document_assembler, sentence_detector])
 
-output = pipeline.fit(sentenceData).transform(sentenceData)
+output = pipeline.fit(sentence_data).transform(sentence_data)
 output.show()
