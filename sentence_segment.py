cac9853a36d245b29af208a35ed7d0644db18543
Ubuntu <ubuntu@ip-10-0-0-5.us-west-2.compute.internal>
2018-05-02 00:08:11 +0000

    load multiple files

--- sentence_segment.py
+++ sentence_segment.py
@@ -3,6 +3,8 @@ import sys
 import os
 import shutil
 import glob
+from os.path import isfile, join
+
 sys.path.extend(glob.glob(os.path.join(os.path.expanduser("~"), ".ivy2/jars/*.jar")))
 from sparknlp.base import DocumentAssembler, Finisher
 from sparknlp.annotator import SentenceDetector
@@ -10,12 +12,13 @@ from sparknlp.common import *
 from pyspark.sql import SparkSession
 from pyspark.ml import Pipeline
 
+TEXT_FOLDER = 'txt'
+
 def main():
     spark = _start_spark()
-    sentence_data = _read_file(spark)
+    sentence_data = _read_files(spark)
     pipeline = _setup_pipeline()
     output = _segment_sentences(sentence_data, pipeline)
-    output.show()
     output.printSchema()
     output.select("sentence").show()
     shutil.rmtree('txt/sentence')
@@ -25,17 +28,27 @@ def _start_spark():
     spark = SparkSession.builder.appName("ner").master("local[1]").config("spark.driver.memory","8G").config("spark.driver.maxResultSize", "2G").config("spark.jar", "lib/sparknlp.jar").config("spark.kryoserializer.buffer.max", "500m").getOrCreate()
     return spark
 
-def _read_file(spark):
-    with open('txt/James Joyce___Ulysses.txt', 'r') as content_file:
-        content = content_file.read().strip()
-        sentence_data = spark.createDataFrame(
-                [
-                    (1, "Hi I heard about Spark. I wish Java could use case classes. Logistic regression models are neat"),
-                    (2, content)
-                ],
-                ["id", "rawDocument"]
-                )
+def _read_files(spark):
+    files = [f for f in os.listdir(TEXT_FOLDER) if isfile(join(TEXT_FOLDER, f))]
+    print(files)
+    sentence_data = spark.createDataFrame(
+            [
+                ("Hi I heard about Spark. I wish Java could use case classes. Logistic regression models are neat", )
+            ],
+            ["rawDocument"]
+            )
+    for textfile in files:
+        new_file = _read_file(spark, textfile)
+        sentence_data = sentence_data.union(new_file)
     return sentence_data
+
+def _read_file(spark, textfile):
+    filepath = '{0}/{1}'.format(TEXT_FOLDER, textfile)
+    print(filepath)
+    with open(filepath, 'r') as content_file:
+        content = content_file.read().replace('\n', '')
+        content = content.replace('\r', '')
+    return spark.createDataFrame([[content]])
 
 def _setup_pipeline():
     document_assembler = DocumentAssembler().setInputCol("rawDocument").setOutputCol("document")
--- sentence_segment.sh
+++ sentence_segment.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-spark-submit --jars ~/.ivy2/jars/JohnSnowLabs_spark-nlp-1.5.1.jar --master spark://ec2-35-155-202-75.us-west-2.compute.amazonaws.com:7077 sentence_segment.py 1000
+# spark-submit --jars ~/.ivy2/jars/JohnSnowLabs_spark-nlp-1.5.2.jar --master spark://ec2-54-190-217-16.us-west-2.compute.amazonaws.com:7077 sentence_segment.py 1000
+
+spark-submit --packages JohnSnowLabs:spark-nlp:1.5.1 --master spark://ec2-54-190-217-16.us-west-2.compute.amazonaws.com:7077 sentence_segment.py 1000
+
+
